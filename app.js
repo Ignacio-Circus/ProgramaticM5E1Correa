d07be07d842dee6076cc02c56eb6bcec5c28af81
ignacio26101985 <nacho.nicolas11@gmail.com>
2021-04-26 16:40:43 -0300

    segundo commit Nacho

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const path = require ("path");
 const puerto = process.env.PORT;
 
 const homeRouter = require('./routes/homeRouter');
-const productRouter = require('./routes/productRouter');
-const userRouter = require('./routes/userRouter');
+//const productRouter = require('./routes/productRouter');
+//const userRouter = require('./routes/userRouter');
 
 app.set ('view engine', 'ejs');
 
@@ -15,7 +15,7 @@ const publicPath = path.join (__dirname, "public")
 app.use (express.static (publicPath));
 
 app.use ('/', homeRouter)
-app.use ('/products', productRouter)
+//app.use ('/products', productRouter)
 //app.use ('/', userRouter)
 
 app.listen (puerto || 3000, function() {
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,4 +1,4 @@
 const express = require('express');
-const router = express.Router();
-const controladorProduct = require('../controller/productController');
+//const router = express.Router();
+//const controladorProduct = require('../controller/productController');
 
